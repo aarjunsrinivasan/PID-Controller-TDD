7fff7e61d20f05bb2011ddb7ccbbd2d04faa924c
aarjunsrinivasan <arjunsrinivasan.a@gmail.com>
2020-09-25 13:20:01 -0400

    updating main function

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,20 @@
+/**
+ * @file    main.cpp
+ * @author  Arjun Srinivasan Ambalam
+ * @copyright GNU Public License
+ * @brief Calls the function to tune controller
+ */
 #include <iostream>
-#include <lib.hpp>
+#include "PIDController.hpp"
 
-int main()
-{
-    dummy();
+int main() {
+    PIDController PID(0.4, 0.3, 0.1);
+    std::cout << "Kp is " << PID.getKp()<< std::endl;
+    std::cout << "Ki is " << PID.getKi()<< std::endl;
+    std::cout << "Kd is " << PID.getKd()<< std::endl;
+
+
+    double tuned = PID.tuneController(15, 12);
+    std::cout << "Controller output velocity is" << tuned << std::endl;
     return 0;
 }
