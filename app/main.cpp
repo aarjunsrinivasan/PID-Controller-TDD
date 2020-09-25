2ff70b6a69b61066e95043b76b0e1e5cc2a98e0a
aarjunsrinivasan <arjunsrinivasan.a@gmail.com>
2020-09-25 13:46:22 -0400

    updating main.cpp for user display

--- app/main.cpp
+++ app/main.cpp
@@ -12,9 +12,13 @@ int main() {
     std::cout << "Kp is " << PID.getKp()<< std::endl;
     std::cout << "Ki is " << PID.getKi()<< std::endl;
     std::cout << "Kd is " << PID.getKd()<< std::endl;
+    double set_point = 15.0;
+    double actual_value = 12.0;
 
+    std::cout << "Desired velocity is" << set_point << std::endl;
+    std::cout << "Present velocity is" << actual_value << std::endl;
 
-    double tuned = PID.tuneController(15, 12);
+    double tuned = PID.tuneController(set_point, actual_value);
     std::cout << "Controller output velocity is" << tuned << std::endl;
     return 0;
 }
